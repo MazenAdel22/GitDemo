447b7c38253d860070180c959233fad946ef17f1
MazenAdel <mazn.adel2070@gmail.com>
2025-09-19 22:51:55 +0300

    My commit after clonning

---
+++ src/test/java/GitClass.java
@@ -0,0 +1,6 @@
+public class GitClass {
+    public static void main(String[] args) {
+        System.out.println("My 1st Editable class");
+        System.out.println("My 2nd Editable class");
+    }
+}

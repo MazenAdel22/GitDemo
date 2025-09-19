b862725ac6cb704bf712a2cb714f147ff7e4be99
MazenAdel <mazn.adel2070@gmail.com>
2025-09-19 23:26:47 +0300

    My Second Editting from JavaOOP Commit

--- src/test/java/GitClass.java
+++ src/test/java/GitClass.java
@@ -2,5 +2,10 @@ public class GitClass {
     public static void main(String[] args) {
         System.out.println("My 1st Editable class");
         System.out.println("My 2nd Editable class");
+        //fsdfsdf
+        //dsfsds
+        //gfdg
+        System.out.println("My 3rd Editable class");
+        System.out.println("My 4th Editable class");
     }
 }
